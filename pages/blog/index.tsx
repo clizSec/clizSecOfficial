527039467b42f08862cfd7f00cabebf6d9cc5d37
Mark Liapustin (clizSec) <mliapustin@trustificorp.com>
2022-09-25 14:53:05 +0300

    Official page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -9,8 +9,8 @@ import { getAllPosts } from 'utils/postsFetcher';
 export default function BlogIndexPage({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <Page
-      title="My SaaS Startup Blog"
-      description="Culpa duis reprehenderit in ex amet cillum nulla do in enim commodo. Sunt ut excepteur et est aliqua anim ea excepteur fugiat voluptate. Fugiat exercitation dolore laboris do quis consectetur eiusmod tempor consequat."
+      title="My Personal Blog"
+      description="Learn more about me, my projects, skills, and get in contact."
     >
       <CustomAutofitGrid>
         {posts.map((singlePost, idx) => (
