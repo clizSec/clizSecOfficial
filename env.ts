bca94f4f95ae0a55c35e1a6348d7158a36ec2232
Mark Liapustin (clizSec) <mliapustin@trustificorp.com>
2022-09-25 14:33:59 +0300

    Change logo

--- env.ts
+++ env.ts
@@ -1,6 +1,6 @@
 export const EnvVars = {
-  SITE_NAME: 'My SaaS Startup',
+  SITE_NAME: 'Mark Liapustin (clizSec) - Personal Blog',
   OG_IMAGES_URL: 'https://next-saas-starter-ashy.vercel.app/og-images/',
-  URL: 'https://next-saas-starter-ashy.vercel.app/',
+  URL: 'https://clizsec.com/',
   MAILCHIMP_SUBSCRIBE_URL: 'https://bstefanski.us5.list-manage.com/subscribe/post?u=66b4c22d5c726ae22da1dcb2e&id=679fb0eec9',
 };
