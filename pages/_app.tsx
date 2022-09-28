9b0436d9b6f33123876e29452872b1146da1e524
Mark Liapustin (clizSec) <mliapustin@trustificorp.com>
2022-09-28 10:24:52 +0300

    Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -44,15 +44,17 @@ function MyApp({ Component, pageProps }: AppProps) {
         {/* <link rel="alternate" type="application/rss+xml" href={EnvVars.URL + 'rss'} title="RSS 2.0" /> */}
       </Head>
 
-      <Script async src="https://www.google-analytics.com/analytics.js" strategy="afterInteractive"></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-      <Script id="google-analytics" strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
           ga('create', 'UA-107382945-2', 'auto');
-          ga('send', 'pageview');`,
-              }}
-      />
+          ga('send', 'pageview');
+        `}
+      </Script>
 
       <ColorModeScript />
       <GlobalStyle />
